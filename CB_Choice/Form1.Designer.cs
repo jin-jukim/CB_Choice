ed0981076c8ee1f2eb097842cb4b69377d10c5ba
Jinju Kim <swinging13@naver.com>
2019-05-24 14:51:05 +0900

    #2 콤보박스 이벤트 처리 코드 구현

--- CB_Choice/Form1.Designer.cs
+++ CB_Choice/Form1.Designer.cs
@@ -41,6 +41,7 @@
             this.cbList.Name = "cbList";
             this.cbList.Size = new System.Drawing.Size(276, 20);
             this.cbList.TabIndex = 0;
+            this.cbList.SelectedIndexChanged += new System.EventHandler(this.CbList_SelectedIndexChanged);
             // 
             // txtList
             // 
@@ -48,6 +49,7 @@
             this.txtList.Name = "txtList";
             this.txtList.Size = new System.Drawing.Size(194, 21);
             this.txtList.TabIndex = 1;
+            this.txtList.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TxtList_KeyPress);
             // 
             // btnAdd
             // 
@@ -57,6 +59,7 @@
             this.btnAdd.TabIndex = 2;
             this.btnAdd.Text = "추가";
             this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.BtnAdd_Click);
             // 
             // lblResult
             // 
@@ -80,6 +83,7 @@
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.Text = "메뉴 선택 추가";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
--- CB_Choice/Form1.cs
+++ CB_Choice/Form1.cs
@@ -12,9 +12,63 @@ namespace CB_Choice
 {
     public partial class Form1 : Form
     {
+        String[] SList = new string[]
+        {
+            "짜장면", "짬뽕", "라조기", "탕수육"
+        };
+        String OrgStr = ""; // 선택 결과 저장
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for(int i=0; i<this.SList.Length; i++)
+            {
+                this.cbList.Items.Add(this.SList[i]);
+            }
+            this.OrgStr = this.lblResult.Text;
+            if(this.cbList.Items.Count > 0)
+            { //cbList에 값이 없을 상황을 방지
+                this.cbList.SelectedIndex = 0; //콤보박스의 첫번째 값을 선택
+            }
+            
+        }
+
+        private void CbList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            this.lblResult.Text = this.OrgStr + this.cbList.Text; //사용자가 콤보박스의 값들을 클릭하면 선택 결과에 나타남
+        }
+
+        private void BtnAdd_Click(object sender, EventArgs e)
+        {
+            Checkinput();
+        }
+
+        private void TxtList_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 13)
+            {
+                Checkinput();
+                e.Handled = true;
+            }
+        }
+
+        private void Checkinput()
+        {
+            if (this.txtList.Text == "")
+            {
+                MessageBox.Show("추가할 항목을 선택하세요", "알림", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                this.txtList.Focus();
+            }
+            else
+            {
+                this.cbList.Items.Add(this.txtList.Text);
+                this.txtList.Text = "";
+                this.txtList.Focus();
+            }
         }
     }
 }
